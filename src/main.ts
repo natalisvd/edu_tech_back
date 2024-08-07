a7fb6f35bbe35e9ceeebc66108d31c0fd71cf70a
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-08-07 09:54:01 +0300

    Change port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParcer from 'cookie-parser';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(cookieParcer());
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+  const port = process.env.PORT || 5000; 
+  await app.listen(port);
 }
 bootstrap();
