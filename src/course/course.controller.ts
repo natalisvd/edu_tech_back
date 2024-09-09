88dcc34c21457ff115c33b7ea3efb1054ecb12d1
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-09-09 16:37:56 +0300

    Create course

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,16 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { CourseService } from './course.service';
-import { CreateCourseDto } from './dto/create-course.dto';
-import { UpdateCourseDto } from './dto/update-course.dto';
 import { Prisma } from '@prisma/client';
-
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as fs from 'fs';
+import * as path from 'path';
+import { resizeAndOptimizeImage } from 'libs/common/src/helpers/resize-image-helper';
 @Controller('courses')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
 
   @Post()
-  create(@Body() createCourseDto: Prisma.CourseCreateInput) {
-    return this.courseService.create(createCourseDto);
+  @UseInterceptors(FileInterceptor('image'))
+  async create(
+    @Body() createCourseDto: Prisma.CourseCreateInput,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    let newCourse = createCourseDto;
+
+    if (image) {
+      const imageDir = path.join(__dirname, '..', '..', '..', 'images');
+      if (!fs.existsSync(imageDir)) {
+        fs.mkdirSync(imageDir, { recursive: true });
+      }
+      const optimizedImageBuffer = await resizeAndOptimizeImage(image.buffer);
+      const fileExt = path.extname(image.originalname);
+      const courseImageName = `courseImage_${Date.now()}${fileExt}`;
+      const courseImagePath = path.join(imageDir, courseImageName);
+
+      fs.writeFileSync(courseImagePath, optimizedImageBuffer);
+      newCourse.courseImageUrl =courseImageName
+    }
+    return this.courseService.create(newCourse);
   }
 
   @Get()
@@ -24,7 +54,10 @@ export class CourseController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCourseDto: Prisma.CourseUpdateInput) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCourseDto: Prisma.CourseUpdateInput,
+  ) {
     return this.courseService.update(id, updateCourseDto);
   }
 
