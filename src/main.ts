fe13cc0e3dc3f06f79a74f06a329c5729450827c
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-10-07 11:54:16 +0300

    Clean

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-import { SeedService } from './seed/seed.service'; // Импортируем SeedService
+import { SeedService } from './seed/seed.service'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Используем cookie-parser middleware для обработки cookies
   app.use(cookieParser());
 
-  // Включаем CORS с нужными настройками, если нужно
   app.enableCors({
-    origin: '*', // укажите разрешенные origins, если необходимо
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept, Authorization', // необходимые заголовки
-    credentials: true, // если вы хотите поддерживать cookies между разными доменами
+    allowedHeaders: 'Content-Type, Accept, Authorization', 
+    credentials: true, 
   });
 
-  // Устанавливаем глобальный префикс для всех маршрутов
   app.setGlobalPrefix('api');
 
-  // Получаем экземпляр SeedService
   const seedService = app.get(SeedService);
 
-  // Выполняем сидирование
   await seedService.runSeed();
 
-  // Слушаем порт, если переменная окружения не установлена, используем 5000
   const port = process.env.PORT || 5000;
   await app.listen(port);
 
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,8 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-// import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  // constructor(private readonly userService: UserService) {
-  //   super();
-  // }
 
   async onModuleInit() {
     console.log('DATABASE_URL:', process.env.DATABASE_URL);
@@ -17,30 +13,8 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
       console.log('Connection successful.');
 
       console.log('Running seed...');
-      // await this.runSeed()
     } catch (error) {
       console.log('Prisma error:', error);
     }
   }
-  // async runSeed() {
-  //   try {
-  //     console.log('Running seed...');
-
-  //     await this.userService.save({
-  //       email: 'testTeamleader@gmail.com',
-  //       password: '123456',
-  //       roles: ['TEAMLEADER'],
-  //     });
-
-  //     await this.userService.save({
-  //       email: 'testDeveloper@gmail.com',
-  //       password: '123456',
-  //       roles: ['DEVELOPER'],
-  //     });
-
-  //     console.log('Seed data has been added.');
-  //   } catch (e) {
-  //     console.error('Error during seeding:', e);
-  //   }
-  // }
 }
