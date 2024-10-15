4a8c5a086a1fabe5a0e4148b40a4ab5e5ed7b804
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-10-15 15:47:58 +0300

    Team docks

--- src/team/dto/create-team.dto.ts
+++ src/team/dto/create-team.dto.ts
@@ -1,27 +1,27 @@
 import { IsString, IsOptional, IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTeamDto {
+  @ApiProperty({
+    description: 'The name of the team',
+    example: 'Team A',
+  })
   @IsString()
   teamName: string;
 
+  @ApiProperty({
+    description: 'ID of the team leader',
+    example: '12345',
+  })
   @IsString()
   teamLeaderId: string;
 
+  @ApiProperty({
+    description: 'Array of participant IDs in the team',
+    example: ['12345', '67890'],
+    required: false,
+  })
   @IsArray()
   @IsOptional()
-  participantIds?: string[];
-}
-
-export class UpdateTeamDto {
-  @IsOptional()
-  @IsString()
-  teamName?: string;
-
-  @IsOptional()
-  @IsString()
-  teamLeaderId?: string;
-
-  @IsOptional()
-  @IsArray()
   participantIds?: string[];
 }
--- src/team/dto/update-team.dto.ts
+++ src/team/dto/update-team.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateTeamDto } from './create-team.dto';
 
-export class UpdateTeamDto extends PartialType(CreateTeamDto) {}
+export class UpdateTeamDto extends PartialType(CreateTeamDto) {
+  @ApiProperty({
+    description: 'The name of the team',
+    example: 'Updated Team A',
+    required: false,
+  })
+  teamName?: string;
+
+  @ApiProperty({
+    description: 'ID of the team leader',
+    example: '12345',
+    required: false,
+  })
+  teamLeaderId?: string;
+
+  @ApiProperty({
+    description: 'Array of participant IDs in the team',
+    example: ['12345', '67890'],
+    required: false,
+  })
+  participantIds?: string[];
+}
