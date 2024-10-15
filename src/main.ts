d68e93f3ac39118096e59e8417d2ae107ed66dc9
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-10-15 15:15:02 +0300

    Swagger init

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { SeedService } from './seed/seed.service'; 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+  .setTitle('Edu Tech')
+  .setDescription('API description')
+  .setVersion('1.0')
+  .addTag('EduTech')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.enableCors({
     origin: '*', 
